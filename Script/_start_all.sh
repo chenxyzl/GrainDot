8fd4b22994b246c9318fd0017b641971e27c9b0c
zhonglin.chen <zhonglin.chen@funplus.com>
2022-02-21 11:35:19 +0800

    ready "add start stop upload script"

--- Script/_start_all.sh
+++ Script/_start_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 launch()
 {
-	./Boot $1 -nodump true -console true > /dev/null 2>&1 &
+	../Bin/Boot $1 -nodump true -console true > /dev/null 2>&1 &
 }
 
 # launch "lobby -id 1"
---
+++ Script/_up_load.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+#cp bin and up to publish
