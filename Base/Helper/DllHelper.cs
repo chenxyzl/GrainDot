ae42ef9a5bf452b901d417daaa6c7a3a732ea6af
lin <ichenzhl@qq.com>
2024-02-01 17:29:20 +0800

    fix 热更新bug，dll占用问题

--- Base/Helper/DllHelper.cs
+++ Base/Helper/DllHelper.cs
@@ -1,18 +1,28 @@
 ﻿using System.IO;
 using System.Reflection;
+using System.Runtime.Loader;
 
 namespace Base.Helper;
 
 public static class DllHelper
 {
+    private static AssemblyLoadContext? _hotfixContext;
+
     public static Assembly[] GetHotfixAssembly(GameServer game)
     {
+        if (_hotfixContext != null)
+        {
+            _hotfixContext.Unload();
+            _hotfixContext = null;
+            //GC.Collect();
+        }
+
+        _hotfixContext = new AssemblyLoadContext("HotfixContext", isCollectible: true);
         Assembly[] assembly =
         {
             typeof(GameServer).Assembly,
             game.GetType().Assembly,
-            Assembly.Load(File.ReadAllBytes($"./{game.Role}.Hotfix.dll"),
-                File.ReadAllBytes($"./{game.Role}.Hotfix.pdb"))
+            _hotfixContext.LoadFromStream(new MemoryStream(File.ReadAllBytes(Path.GetFullPath($"./{game.Role}.Hotfix.dll"))),new MemoryStream(File.ReadAllBytes(Path.GetFullPath($"./{game.Role}.Hotfix.pdb"))))
         };
 
         return assembly;
